413f072b4d01eebe632f7b9831d0b28c8976d138
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-10-20 20:30:29 +0300

    done

--- skypro-fitness/tailwind.config.js
+++ skypro-fitness/tailwind.config.js
@@ -5,8 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'yoga': "url('/public/yogapic.png')",
+        'vector':"url('/public/vector.png')",
+    },
   },
   plugins: [],
 }
-
+}
