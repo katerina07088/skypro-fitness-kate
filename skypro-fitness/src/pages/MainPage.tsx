b448751934533e1e2c6b8474d4cde5d4da96703b
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-10-25 08:08:42 +0300

    partly ready

--- skypro-fitness/src/pages/MainPage.tsx
+++ skypro-fitness/src/pages/MainPage.tsx
@@ -1,7 +1,9 @@
+import Header from "../components/Header/Header";
+
 export default function MainPage() {
-    return (
-      <>
-        <h1 className="text-10xl font-bold underline">Main Page</h1>
-      </>
-    );
-  }
+  return (
+    <>
+      <Header isAuth={false}/>
+    </>
+  );
+}
--- skypro-fitness/tailwind.config.js
+++ skypro-fitness/tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'yoga': "url('/public/yogapic.png')",
-        'vector':"url('/public/vector.png')",
+    container: {
+      center: true,
     },
+    extend: {
+      colors: {
+        gray: "#999999",
+        "light-gray": "#F7F7F7",
+        green: "#BCEC30",
+        "light-green": "#C6FF00",
+      },
+      backgroundImage: {
+        yoga: "url('public/img/yogapic.png')",
+        vector: "url('public/img/vector.png')",
+        yogamin: "url('public/img/yogapicmin.png')",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-}
+};
