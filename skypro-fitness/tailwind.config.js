12a56fb3cff5199d4e32f8523eda8d926abd21b9
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-10-30 16:12:20 +0300

    corrected

--- skypro-fitness/tailwind.config.js
+++ skypro-fitness/tailwind.config.js
@@ -17,8 +17,6 @@ export default {
       },
       backgroundImage: {
         yoga: "url('/public/img/yogapic.png')",
-        vector: "url('/public/img/vector.png')",
-        vectormin: "url('/public/img/vectormin.png')",
         yogamin: "url('/public/img/yogapicmin.png')",
       },
     },
