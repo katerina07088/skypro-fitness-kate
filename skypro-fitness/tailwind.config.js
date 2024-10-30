34dcf725ac81f565f67a7f41f3fb52fe9e2590d0
Gazim <akbutin.gazim.2000@gmail.com>
2024-10-30 17:44:40 +0500

    1.5

--- skypro-fitness/tailwind.config.js
+++ skypro-fitness/tailwind.config.js
@@ -6,14 +6,19 @@ export default {
   ],
   theme: {
     colors:{
-      black: '#000000',
-      while: '#FFFFFF',
-      bgclr: '#FAFAFA',
-      custom: '#f7f7f7',
-      btnbg: '#BCEC30',
-      lightBlue: '#00C1FF',
-      blackRgba: 'rgba(0, 0, 0, 0.54)',
-      inputBorder:"#D0CECE"
+      "gray": "#999999",
+      "light-gray": "#F7F7F7",
+      "green": "#BCEC30",
+      "light-green": "#C6FF00",
+      "blue": "#00C1FF",
+      "black": "#000000",
+      "while": "#FFFFFF",
+      "bgclr": "#FAFAFA",
+      "custom": "#f7f7f7",
+      "btnbg": "#BCEC30",
+      "lightBlue": "#00C1FF",
+      "blackRgba": "rgba(0, 0, 0, 0.54)",
+      "inputBorder": "#D0CECE",
     },
     extend: {
       
