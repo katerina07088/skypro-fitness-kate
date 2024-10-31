d15160e2be853cb7389144dc41117f2073691056
Gazim <akbutin.gazim.2000@gmail.com>
2024-10-31 22:52:33 +0500

    1.6

--- skypro-fitness/tailwind.config.js
+++ skypro-fitness/tailwind.config.js
@@ -1,29 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      "gray": "#999999",
+    colors: {
+      gray: "#999999",
       "light-gray": "#F7F7F7",
-      "green": "#BCEC30",
+      green: "#BCEC30",
       "light-green": "#C6FF00",
-      "blue": "#00C1FF",
-      "black": "#000000",
-      "while": "#FFFFFF",
-      "bgclr": "#FAFAFA",
-      "custom": "#f7f7f7",
-      "btnbg": "#BCEC30",
-      "lightBlue": "#00C1FF",
-      "blackRgba": "rgba(0, 0, 0, 0.54)",
-      "inputBorder": "#D0CECE",
+      blue: "#00C1FF",
+      black: "#000000",
+      while: "#FFFFFF",
+      bgclr: "#FAFAFA",
+      custom: "#f7f7f7",
+      btnbg: "#BCEC30",
+      lightBlue: "#00C1FF",
+      blackRgba: "rgba(0, 0, 0, 0.54)",
+      inputBorder: "#D0CECE",
     },
     extend: {
-      
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      extend: {
+        colors: {
+          gray: "#999999",
+          "light-gray": "#F7F7F7",
+          green: "#BCEC30",
+          "light-green": "#C6FF00",
+          blue: "#00C1FF",
+        },
+        backgroundImage: {
+          yoga: "url('/public/img/yogapic.png')",
+          yogamin: "url('/public/img/yogapicmin.png')",
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
